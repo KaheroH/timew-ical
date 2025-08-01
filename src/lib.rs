99577918e4c109669f566d653dbeccf2430a9eb8
kharriot <kharriot-ex@epichosted.com>
2025-08-01 17:08:07 -0500

    Worked, but want to deserialize string into DateTime<Utc>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
-use chrono::{DateTime, FixedOffset};
+use chrono::{DateTime, FixedOffset, Utc};
 use std::process::Command;
+use serde::{Deserialize, Serialize};
 
 #[derive(Parser)]
 pub struct Arguments {
@@ -9,56 +10,87 @@ pub struct Arguments {
 }
 
 #[derive(Debug)]
-pub struct TimewOutBytes {
-    pub output: Vec<u8>,
-    pub error: Vec<u8>,
+pub enum TimewOutBytes {
+    Output(Vec<u8>),
+    Error(Vec<u8>),
 }
 impl TimewOutBytes {
     pub fn new(start: DateTime<FixedOffset>, end: DateTime<FixedOffset>) -> Self {
         let command_output = Command::new("timew")
             .args([
                 "export",
-                &start.date_naive().format("%Y-%m-%d").to_string(),
-                &end.date_naive().format("%Y-%m-%d").to_string(),
+                &start.format("%Y-%m-%d").to_string(),
+                "-",
+                &end.format("%Y-%m-%d").to_string(),
             ])
             .output()
             .expect("Failed to execute timew process with args");
 
         let output = command_output.stdout;
         let error = command_output.stderr;
-        
-        TimewOutBytes { output, error }
+    
+        if !error.is_empty() { TimewOutBytes::Error(error) }
+        else { TimewOutBytes::Output(output) }
     }
 }
 impl From<TimewOutBytes> for TimewOutString {
     fn from(bytes: TimewOutBytes) -> Self {
-        let output = match String::from_utf8(bytes.output) {
-            Ok(s) => s.trim_end().to_string(),
-            Err(e) => panic!("timew output contains invalid utf8 characters. timew-ical cannot yet handle these: {e}"),
-        };
-        let error = match String::from_utf8(bytes.error) {
-            Ok(s) => s.trim_end().to_string(),
-            Err(e) => panic!("The Error stream from timew contained invalid utf8 characters: {e}"),
-        };
-
-        TimewOutString { output, error }
+        match bytes {
+            TimewOutBytes::Output(out) => match String::from_utf8(out) {
+                    Ok(s) => TimewOutString::Output(s.trim_end().to_string()),
+                    Err(e) => panic!("The Standard Output stream from timew contained invalid utf8 characters: {e}"),
+                },
+            TimewOutBytes::Error(err) => match String::from_utf8(err) {
+                    Ok(s) => TimewOutString::Error(s.trim_end().to_string()),
+                    Err(e) => panic!("The Error stream from timew contained invalid utf8 characters: {e}"),
+                },
+        }
     }
 }
 
 #[derive(Debug)]
-pub struct TimewOutString {
-    pub output: String,
-    pub error: String,
+pub enum TimewOutString {
+    Output(String),
+    Error(String),
 }
 
-#[derive(Debug)]
-pub struct TimeTrackingEntry {}
-#[derive(Debug)]
+#[derive(Debug, Deserialize, Serialize)]
+pub struct TimeTrackingEntry {
+    id: i64,
+    #[serde(with = "date_format")]
+    start: DateTime<Utc>,
+    #[serde(with = "date_format")]
+    end: DateTiem<Utc>,
+    tags: Vec<String>,
+}
+mod date_format {
+    use chrono::{DateTime, Utc, NaiveDateTime};
+    use serde::{self, Deserialize, Serializer, Deserializer};
+
+    const FORMAT: &'static str = "%Y-%m-%dT%H%M%SZ";
+
+    pub fn serialize<S>(
+        date: &DateTime<Utc>,
+        serializer: S
+    ) -> Result<S::Ok, S::Error> 
+    where
+        S: Serializer,
+    {
+        let s = format!("{}", date.format(FORMAT));
+        serializer.serialize_str(&s)
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
 pub struct TimeData {
    pub entries: Vec<TimeTrackingEntry>, 
 }
 
 impl From<TimewOutString> for TimeData {
     fn from(timew_string: TimewOutString) -> Self {
+        match timew_string {
+            TimewOutString::Output(out) => TimeData { entries: serde_json::from_str(&out).unwrap() },
+            TimewOutString::Error(err) => panic!("timew Error: {err}"),
+        }
     }
 }
